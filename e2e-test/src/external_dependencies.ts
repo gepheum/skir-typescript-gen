ba28fc79948d00b0fe62fbe9b46a3b0d036b28e7
Tyler Fibonacci <gepheum@gmail.com>
2026-01-26 06:28:55 -0500

    add UTs for external dependencies support

---
+++ e2e-test/src/external_dependencies.ts
@@ -0,0 +1,9 @@
+import { describe, it } from "mocha";
+import { Quest } from "../skirout/@gepheum/fantasy-game-skir-example/fantasy_game.js";
+import { QuestCollection } from "../skirout/quest_collection.js";
+
+describe("external dependencies", () => {
+  it("access", () => {
+    [Quest, QuestCollection];
+  });
+});
